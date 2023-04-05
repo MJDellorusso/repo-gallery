85137c41f6496ad454554f847afbe27fe3d1ecc8
marty_d <martydellorusso@gmail.com>
2023-04-05 05:42:54 -0400

    Created function to display profile info

--- js/script.js
+++ js/script.js
@@ -6,9 +6,21 @@ const getProfile = async function () {
   const profileFetch = await fetch(`https://api.github.com/users/${username}`);
   const profileInfo = await profileFetch.json();
   console.log(profileInfo);
+  displayProfile(profileInfo);
 };
 getProfile();
 
 const displayProfile = function (profileInfo) {
-  const profileDiv = document.createElement(".user-info");
+  const profileDiv = document.createElement("div");
+  profileDiv.classList.add("user-info");
+  profileDiv.innerHTML = `<figure>
+    <img alt="user avatar" src=${profileInfo.avatar_url} />
+  </figure>
+  <div>
+    <p><strong>Name:</strong> ${profileInfo.name}</p>
+    <p><strong>Bio:</strong> ${profileInfo.bio}</p>
+    <p><strong>Location:</strong> ${profileInfo.location}</p>
+    <p><strong>Number of public repos:</strong> ${profileInfo.public_repos}</p>
+  </div> `;
+  overview.append(profileDiv);
 };
